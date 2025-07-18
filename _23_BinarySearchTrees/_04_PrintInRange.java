1d11c54e538e49b5adb960efc149be4376b0cb6e
DipanPramanik098 <myselfdipan6@gmail.com>
2025-07-18 11:15:36 +0530

    Print In Range

---
+++ _23_BinarySearchTrees/_04_PrintInRange.java
@@ -0,0 +1,46 @@
+public class _04_PrintInRange {
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void inOrder(Node root) {
+        if (root == null)
+            return;
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    public static void printInRange(Node root , int k1, int k2){
+        if(root == null) return;
+        if(root.data>=k1 && root.data<=k2){
+            printInRange(root.left, k1, k2);
+            System.out.println(root.data);
+            printInRange(root.right, k1, k2);
+        }else if(root.data < k1){
+            printInRange(root.left, k1, k2);
+        }else{
+            printInRange(root.right, k1, k2);
+        }
+    }
+    public static void main(String[] args) {
+        Node root = new Node(8);
+        root.left = new Node(5);
+        root.right = new Node(10);
+        root.left.left = new Node(3);
+        root.left.right = new Node(6);
+        root.right.right = new Node(11);
+
+        int k1 = 5, k2 = 12;
+        printInRange(root,k1,k2); 
+    }
+
+}

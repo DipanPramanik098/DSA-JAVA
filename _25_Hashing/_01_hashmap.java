3ef4e95cf79b0ac2aa54ef7588d98358b7c270a3
DipanPramanik098 <myselfdipan6@gmail.com>
2025-07-20 22:00:58 +0530

    HashMap

---
+++ _25_Hashing/_01_hashmap.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class _01_hashmap {
+    public static void main(String[] args) {
+        HashMap<String,Integer>hm = new HashMap<>();
+
+        // put
+        hm.put("Dipan",1);
+        hm.put("A",10);
+        hm.put("B",101);
+
+        System.out.println(hm);
+
+        // get
+        System.out.println(hm.get("Dipan"));
+
+        // containsKey
+        System.err.println(hm.containsKey("A"));
+        System.err.println(hm.containsKey("VVV"));
+
+        // remove
+        hm.remove("A");
+        System.out.println(hm);
+    }    
+}

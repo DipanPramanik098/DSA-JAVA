f4a4f1dabe60bd7e997668d49e537edb59ef1d86
DipanPramanik098 <myselfdipan6@gmail.com>
2025-07-24 08:54:56 +0530

    prefix Problem

--- _13_Recursion/_01_PrintNtoOne.java
+++ _13_Recursion/_01_PrintNtoOne.java
@@ -1,14 +1,15 @@
 public class _01_PrintNtoOne {
-    public static void printDec(int n){
-        if(n==1){
+    public static void printDec(int n) {
+        if (n == 1) {
             System.out.println(n);
             return;
         }
-        System.out.println(n+" ");
-        printDec(n-1);
+        System.out.println(n + " ");
+        printDec(n - 1);
     }
+
     public static void main(String[] args) {
-        int n=10;
+        int n = 10;
         printDec(n);
-    }    
+    }
 }

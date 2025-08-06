a81528583fb74894eed8b2b1f70e96b1f8b36fc1
DipanPramanik098 <myselfdipan6@gmail.com>
2025-08-06 12:00:46 +0530

    Basic Patterns

---
+++ _02_Patterns/pattern5.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class pattern5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int row = 1;
+		int star = 1;
+		int space = n - 1;
+		while (row <= n) {
+			// space
+			int i = 1;
+			while (i <= space) {
+				System.out.print("  ");
+				i++;
+			}
+			// star
+			int j = 1;
+			while (j <= star) {
+				System.out.print("* ");
+				j++;
+			}
+			// next Row Prep
+			System.out.println();
+			row++;
+			star += 2;
+			space--;
+
+		}
+    }
+}

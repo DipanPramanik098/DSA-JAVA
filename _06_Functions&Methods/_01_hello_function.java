d639e35dd2fa4a2f0bd82b3a5693606185a51869
DipanPramanik098 <myselfdipan6@gmail.com>
2025-08-08 14:28:24 +0530

    Function

---
+++ _06_Functions&Methods/_01_hello_function.java
@@ -0,0 +1,10 @@
+public class _01_hello_function {
+
+    // 
+    public static void print(){
+        System.out.println("Namaste Function");
+    }
+     public static void main(String[] args) {
+        print();
+     }
+}
---
+++ _06_Functions&Methods/_02_add.java
@@ -0,0 +1,9 @@
+public class _02_add {
+
+    public static int sum(int a, int b) {
+        return a + b; // Return the sum of a and b
+    }   
+    public static void main(String[] args) {
+        System.out.println(sum(19, 20)); // Call the sum function and print the result
+    }
+}

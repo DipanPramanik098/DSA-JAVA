e141c0f7e4da5f61c66632fcbe16b18d80c6fb11
DipanPramanik098 <myselfdipan6@gmail.com>
2025-08-11 15:47:51 +0530

    BS

---
+++ _09_BinarySearch/_06_BoolAllocation.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class _06_BoolAllocation {
+    public static void main(String[] args) {
+        int pages [] = {12,64,67,90};
+        int nos = 3; //no of student
+
+        System.out.println(MinPage(pages, nos));
+    }
+    public static int MinPage(int [] pages , int nos){
+        Arrays.sort(pages);
+
+        int lo = 0;
+        int hi = 0;
+        for(int i=0;i<pages.length; i++){
+            hi+=pages[i];
+        }
+        int ans = 0;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo)/2;
+            if(isPossible(pages,nos,mid)){
+                ans = mid;
+                hi = mid-1;
+            }else{
+                lo = mid+1;
+            }
+        }
+        return ans;
+    }
+
+    public static boolean isPossible(int [] pages, int nos, int mid){
+        int student = 1;
+        int readPage = 0;
+        for(int i=0; i<pages.length;i++){
+            if(readPage + pages[i] <= mid){
+                readPage += pages[i];
+            }else{
+                student++;
+                readPage = pages[i];
+            }
+
+            if(student > nos) return false;
+        }
+        return true;
+    }
+}

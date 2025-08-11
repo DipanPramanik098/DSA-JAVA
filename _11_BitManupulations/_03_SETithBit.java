e7bae50cda4402f3639df0f1c1ef4626437d2db1
DipanPramanik098 <myselfdipan6@gmail.com>
2025-08-11 11:47:21 +0530

    --

--- _11_BitManupulations/_03_SETithBit.java
+++
@@ -1,9 +0,0 @@
-public class _03_SETithBit {
-    public static int setithBit(int num, int i) {
-        int mask = 1 << i;
-        return num | mask;
-    }
-    public static void main(String[] args) {
-        System.out.println(setithBit(10,2));
-    }
-}
--- _11_BitManupulations/_08_checkPowerOfTwo.java
+++
@@ -1,9 +0,0 @@
-public class _08_checkPowerOfTwo {
-    public static boolean powOfTwo(int n){
-        return (n&(n-1))==0;
-    }
-    public static void main(String[] args) {
-        System.out.println(powOfTwo(15));
-        System.out.println(powOfTwo(16));
-    }
-}

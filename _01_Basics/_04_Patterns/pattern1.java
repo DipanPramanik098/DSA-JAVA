4f7086840c4aeb26ba5a944fa7997f33e346f180
DipanPramanik098 <myselfdipan6@gmail.com>
2025-08-24 18:46:32 +0530

    Pattern -1

---
+++ _01_Basics/_04_Patterns/pattern1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class pattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int row = 1;
+        while(row <= n){
+            int star = 1;
+            while(star <= n){
+                System.out.print("*");
+                star++;
+            }
+            System.out.println();
+            row++;
+        }
+    }
+}

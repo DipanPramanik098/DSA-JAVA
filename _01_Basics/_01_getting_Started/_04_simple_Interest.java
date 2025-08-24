22f3db2476dc7b964551ddfc475349c081f568b3
DipanPramanik098 <myselfdipan6@gmail.com>
2025-08-24 18:32:42 +0530

    basics

---
+++ _01_Basics/_01_getting_Started/_04_simple_Interest.java
@@ -0,0 +1,11 @@
+
+public class _04_simple_Interest {
+    public static void main(String[] args) {
+        int p = 1000;
+        int r = 5;
+        int t = 1;
+        int si = (p * r * t) / 100;
+        System.out.println(si);
+
+    }    
+}

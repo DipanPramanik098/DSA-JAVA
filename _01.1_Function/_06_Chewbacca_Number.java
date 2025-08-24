4b7f2c5fa13979b02b7357299c35331ab65cd9cb
DipanPramanik098 <myselfdipan6@gmail.com>
2025-08-24 19:48:13 +0530

    Function

--- _01.1_Function/_06_Chewbacca_Number.java
+++ _01.1_Function/_06_Chewbacca_Number.java
@@ -1,31 +1,32 @@
 import java.util.*;
+
 public class _06_Chewbacca_Number {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long n = sc.nextLong();
         System.out.println(Chewbacca(n));
-
     }
 
     public static long Chewbacca(long n) {
-        long sum = 0;
-        long mul = 1;
-        while (n > 9) {
-            long rem = n % 10;
-            if (rem >= 5) {
-                rem = 9 - rem;
-            }
-            sum = sum + rem * mul;
-            mul = mul * 10;
-            n = n / 10;
-        }
-        // last digit
-        if (n <= 4 || n == 9) {
-            sum = sum + n * mul;
-        } else {
-            sum = sum + (9 - n) * mul;
-        }
-        return sum;
+        String num = Long.toString(n);
+        StringBuilder sb = new StringBuilder();
 
+        for (int i = 0; i < num.length(); i++) {
+            int digit = num.charAt(i) - '0';
+
+            // Only change if digit >= 5
+            if (digit >= 5) {
+                digit = 9 - digit;
+            }
+
+            // First digit must not be zero
+            if (i == 0 && digit == 0) {
+                digit = num.charAt(i) - '0';
+            }
+
+            sb.append(digit);
+        }
+
+        return Long.parseLong(sb.toString());
     }
 }

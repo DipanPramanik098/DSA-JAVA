eb34f898c60eb98c241f031524c55690e2c2b926
DipanPramanik098 <myselfdipan6@gmail.com>
2025-09-01 10:32:40 +0530

    Basics Sorting

--- _02_One_Dimentional_Array/class02_Basic_Sorting/_04_upperBound.java
+++ _02_One_Dimentional_Array/class02_Basic_Sorting/_04_upperBound.java
@@ -1,31 +1,32 @@
 public class _04_upperBound {
 
-    // Function to find upper bound using binary search
+    // Function to find index of x or -1 if not present
     public static int upperBound(int[] arr, int x) {
-        int low = 0, high = arr.length; // high = n (not n-1)
+        int low = 0, high = arr.length - 1;
+        int result = -1;
 
-        while (low < high) {
+        while (low <= high) {
             int mid = low + (high - low) / 2;
 
-            if (arr[mid] <= x) {
-                // Move right if arr[mid] is less than or equal to x
-                low = mid + 1;
+            if (arr[mid] == x) {
+                result = mid; // Found x
+                break; // Stop since we only want the index of x
+            } else if (arr[mid] < x) {
+                low = mid + 1; // Move right
             } else {
-                // Candidate found, but look left for smaller index
-                high = mid;
+                high = mid - 1; // Move left
             }
         }
 
-        return low; // Upper bound index
+        return result;
     }
 
     public static void main(String[] args) {
         int[] arr = { 2, 4, 6, 8, 10 };
 
-        int x1 = 6, x2 = 7, x3 = 10;
+        int x1 = 6, x2 = 7;
 
-        System.out.println("Upper bound of " + x1 + " is at index: " + upperBound(arr, x1));
-        System.out.println("Upper bound of " + x2 + " is at index: " + upperBound(arr, x2));
-        System.out.println("Upper bound of " + x3 + " is at index: " + upperBound(arr, x3));
+        System.out.println("Index of " + x1 + " is: " + upperBound(arr, x1));
+        System.out.println("Index of " + x2 + " is: " + upperBound(arr, x2));
     }
 }

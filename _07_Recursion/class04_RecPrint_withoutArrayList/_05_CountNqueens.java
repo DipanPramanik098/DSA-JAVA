dadb026dd177261942a794d7f59f2dc6db07868a
DipanPramanik098 <myselfdipan6@gmail.com>
2025-09-06 09:19:05 +0530

    Recursion Print

--- _07_Recursion/class04_RecPrint_withoutArrayList/_05_CountNqueens.java
+++ _07_Recursion/class04_RecPrint_withoutArrayList/_05_CountNqueens.java
@@ -1,50 +1,45 @@
-public class _05_CountNqueens {     
-    public static int countNqueen(boolean [][] board, int row){
-        if(row == board.length) return 1;
+public class _05_CountNqueens {
+
+    public static int countNqueen(boolean[][] board, int row) {
+        // ✅ Base case: all queens placed
+        if (row == board.length)
+            return 1;
+
         int count = 0;
-        for(int col = 0; col <board[row].length; col++){
-            if(isSafe(board, row, col)){
-                board[row][col] = true;
-                count += countNqueen(board, row+1);
-                board[row][col] = false;
+        for (int col = 0; col < board[row].length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = true; // place queen
+                count += countNqueen(board, row + 1);
+                board[row][col] = false; // backtrack
             }
         }
         return count;
     }
-    public static boolean isSafe(boolean board[][], int row, int col){
-        //todo: check row
-        for(int j = col; j>=0; j--){
-            if(board[row][j] == true) return false;
+
+    public static boolean isSafe(boolean[][] board, int row, int col) {
+        // ✅ Check column above
+        for (int i = 0; i < row; i++) {
+            if (board[i][col])
+                return false;
         }
-        //todo: check diagonals
-        // note: upper left
-        for(int i = row,j=col; i>=0&&j>=0; i--,j--){
-            if(board[i][j]) return false;
-        }
-        // note: upper right
-        for (int i = row, j = col; i >=0 && j <board[i].length; i--, j++) {
+
+        // ✅ Check upper-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
             if (board[i][j])
                 return false;
         }
-        // note: lower left
-        for (int i = row, j = col; i < board.length && j >= 0; i++, j--) {
+
+        // ✅ Check upper-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
             if (board[i][j])
                 return false;
         }
-        // note: lower  right
-        for (int i = row, j = col; i < board.length && j < board[i].length; i++, j++) {
-            if (board[i][j])
-                return false;
-        }
-        //todo: check col
-        for(int i=row; i>=0; i--){
-            if(board[i][col] == true) return false;
-        }
+
         return true;
     }
+
     public static void main(String[] args) {
-        boolean [][] board = new boolean[4][4];
-        
-        System.out.println(countNqueen(board,0));
-    }    
+        boolean[][] board = new boolean[4][4];
+        System.out.println(countNqueen(board, 0)); // ✅ Output: 2 (for 4×4 board)
+    }
 }
--- _07_Recursion/class04_RecPrint_withoutArrayList/_06_printNqueens.java
+++ _07_Recursion/class04_RecPrint_withoutArrayList/_06_printNqueens.java
@@ -1,57 +1,45 @@
-
-
 public class _06_printNqueens {
     public static void printNqueen(boolean[][] board, int row, String ans) {
-        if (row == board.length){
+        // ✅ Base case: if all rows are filled, print solution
+        if (row == board.length) {
             System.out.println(ans);
             return;
         }
+
+        // Try placing queen in each column
         for (int col = 0; col < board[row].length; col++) {
             if (isSafe(board, row, col)) {
-                board[row][col] = true;
-                printNqueen(board, row+1, ans+"{"+(row+1)+" , "+(col+1)+"}");
-                board[row][col] = false;
+                board[row][col] = true; // place queen
+                printNqueen(board, row + 1, ans + "{" + (row + 1) + " , " + (col + 1) + "} ");
+                board[row][col] = false; // backtrack
             }
         }
     }
 
-    public static boolean isSafe(boolean board[][], int row, int col) {
-        // todo: check row
-        for (int j = col; j >= 0; j--) {
-            if (board[row][j] == true)
+    public static boolean isSafe(boolean[][] board, int row, int col) {
+        // ✅ Check column above
+        for (int i = 0; i < row; i++) {
+            if (board[i][col])
                 return false;
         }
-        // todo: check diagonals
-        // note: upper left
-        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+
+        // ✅ Check upper-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
             if (board[i][j])
                 return false;
         }
-        // note: upper right
-        for (int i = row, j = col; i >= 0 && j < board[i].length; i--, j++) {
+
+        // ✅ Check upper-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
             if (board[i][j])
                 return false;
         }
-        // note: lower left
-        for (int i = row, j = col; i < board.length && j >= 0; i++, j--) {
-            if (board[i][j])
-                return false;
-        }
-        // note: lower right
-        for (int i = row, j = col; i < board.length && j < board[i].length; i++, j++) {
-            if (board[i][j])
-                return false;
-        }
-        // todo: check col
-        for (int i = row; i >= 0; i--) {
-            if (board[i][col] == true)
-                return false;
-        }
-        return true;
+
+        return true; // safe to place queen
     }
 
     public static void main(String[] args) {
-        boolean[][] board = new boolean[4][4];  
+        boolean[][] board = new boolean[4][4];
         printNqueen(board, 0, "");
     }
 }
